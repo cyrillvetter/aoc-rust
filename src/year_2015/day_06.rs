cb4b55d0f2f3dae5948b33cdcbfd523e5a66adee
Cyrill Vetter <cyrill.vetter@gmail.com>
2022-11-30 18:04:40 +0100

    2015 day 6 part 2

--- src/year_2015/day_06.rs
+++ src/year_2015/day_06.rs
@@ -19,17 +19,18 @@ pub fn part_one() -> String {
 
         for y in grid.iter_mut().skip(from_y).take(to_y - from_y + 1) {
             for x in y.iter_mut().skip(from_x).take(to_x - from_x + 1) {
-                let prev = x.clone();
-
-                *x = match action {
+                let prev = *x;
+                let next = match action {
                     "turn on" => true,
                     "turn off" => false,
-                    _ => !(*x),
+                    _ => !prev,
                 };
 
-                if !prev && *x {
+                *x = next;
+
+                if !prev && next {
                     counter += 1;
-                } else if prev && !*x {
+                } else if prev && !next {
                     counter -= 1;
                 }
             }
@@ -40,5 +41,38 @@ pub fn part_one() -> String {
 }
 
 pub fn part_two() -> String {
-    String::new()
+    let lines = read_lines(2015, 6);
+    let mut grid = vec![vec![0u32; 1000]; 1000];
+
+    let instr_regex = Regex::new("(turn off|turn on|toggle)\\s(\\d+),(\\d+)\\sthrough\\s(\\d+),(\\d+)").unwrap();
+    let mut counter = 0;
+
+    for l in lines {
+        let capts = instr_regex.captures(&l).unwrap();
+
+        let action = &capts[1];
+        let from_x = capts[2].parse::<usize>().unwrap();
+        let from_y = capts[3].parse::<usize>().unwrap();
+        let to_x = capts[4].parse::<usize>().unwrap();
+        let to_y = capts[5].parse::<usize>().unwrap();
+
+        for y in grid.iter_mut().skip(from_y).take(to_y - from_y + 1) {
+            for x in y.iter_mut().skip(from_x).take(to_x - from_x + 1) {
+                let prev = *x;
+                let next = match action {
+                    "turn on" => prev + 1,
+                    "turn off" => prev.checked_sub(1).unwrap_or(0u32),
+                    _ => prev + 2,
+                };
+
+                *x = next;
+
+                if next != prev {
+                    counter += next - prev;
+                }
+            }
+        }
+    }
+
+    counter.to_string()
 }
