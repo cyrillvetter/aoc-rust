4e069b4d87a0a7484750a11519c753b3d327c1e0
Cyrill Vetter <cyrill.vetter@gmail.com>
2022-12-01 23:44:27 +0100

    days 2 and 3 added

---
+++ src/year_2022/day_02.rs
@@ -0,0 +1,24 @@
+use crate::solution::Solution;
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Solution {
+    Solution::Str(String::from("Not implemented"))
+}
+
+pub fn part_two(input: &str) -> Solution {
+    Solution::Str(String::from("Not implemented"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::solution::Solution;
+    use crate::common::read_string;
+
+    #[test]
+    fn check() {
+        let input = read_string(2022, 3);
+        assert_eq!(part_one(&input), Solution::I32(70613));
+        assert_eq!(part_two(&input), Solution::I32(205805));
+    }
+}
--- src/year_2022/mod.rs
+++ src/year_2022/mod.rs
@@ -9,8 +9,12 @@ pub fn solve_all() {
 pub fn solve_day(day: u8) {
     match day {
         1 => solve_parts(2022, day, day_01::part_one, day_01::part_two),
+        2 => solve_parts(2022, day, day_02::part_one, day_02::part_two),
+        3 => solve_parts(2022, day, day_03::part_one, day_03::part_two),
         _ => return,
     };
 }
 
 mod day_01;
+mod day_02;
+mod day_03;
