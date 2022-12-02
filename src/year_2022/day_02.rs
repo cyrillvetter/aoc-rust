d66fca3ddd621ae6d8b4372dd128632bf363d5a0
Cyrill Vetter <cyrill.vetter@gmail.com>
2022-12-02 08:13:28 +0100

    2022 day 2

--- src/year_2022/day_02.rs
+++ src/year_2022/day_02.rs
@@ -1,12 +1,50 @@
 use crate::solution::Solution;
-use itertools::Itertools;
 
 pub fn part_one(input: &str) -> Solution {
-    Solution::Str(String::from("Not implemented"))
+    let mut score = 0;
+
+    for line in input.lines() {
+        let split: (&str, &str) = line.split_at(1);
+
+        let a_score = get_choice_score(split.0);
+        let mut b_score = get_choice_score(split.1.trim());
+        if a_score == b_score {
+            b_score += 3;
+        } else if a_score == 1 && b_score == 2 || a_score == 2 && b_score == 3 || a_score == 3 && b_score == 1 {
+            b_score += 6;
+        }
+
+        score += b_score;
+    }
+
+    Solution::U32(score)
 }
 
 pub fn part_two(input: &str) -> Solution {
-    Solution::Str(String::from("Not implemented"))
+    let mut score = 0;
+
+    for line in input.lines() {
+        let split: (&str, &str) = line.split_at(1);
+
+        let a_score = get_choice_score(split.0);
+        score += match split.1.trim() {
+            "X" => if a_score > 1 { a_score - 1 } else { 3 },
+            "Y" => a_score + 3,
+            "Z" => (if a_score < 3 { a_score + 1 } else { 1 }) + 6,
+            _ => unreachable!()
+        }
+    }
+
+    Solution::U32(score)
+}
+
+fn get_choice_score(choice: &str) -> u32 {
+    match choice {
+        "A" | "X" => 1,
+        "B" | "Y" => 2,
+        "C" | "Z" => 3,
+        _ => unreachable!()
+    }
 }
 
 #[cfg(test)]
@@ -17,8 +55,8 @@ mod tests {
 
     #[test]
     fn check() {
-        let input = read_string(2022, 3);
-        assert_eq!(part_one(&input), Solution::I32(70613));
-        assert_eq!(part_two(&input), Solution::I32(205805));
+        let input = read_string(2022, 2);
+        assert_eq!(part_one(&input), Solution::U32(11767));
+        assert_eq!(part_two(&input), Solution::U32(13886));
     }
 }
