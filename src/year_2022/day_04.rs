79cda3ef935d78403ea9d35c04b71b579fe16640
Cyrill Vetter <cyrill.vetter@gmail.com>
2022-12-04 12:37:17 +0100

    2022 day 4

--- src/year_2022/day_04.rs
+++ src/year_2022/day_04.rs
@@ -1,11 +1,35 @@
 use crate::solution::Solution;
+use itertools::Itertools;
 
 pub fn part_one(input: &str) -> Solution {
-    Solution::Empty
+    let fully_contains_count = input
+        .lines()
+        .map(|l| l.split(","))
+        .map(|s| s.map(|i| i.split("-").map(|j| j.parse::<i32>().unwrap()).collect_tuple::<(_, _)>().unwrap()).collect_vec())
+        .filter(|r| fully_contains(r[0], r[1]))
+        .count();
+
+    Solution::USize(fully_contains_count)
 }
 
 pub fn part_two(input: &str) -> Solution {
-    Solution::Empty
+    let overlaps_count = input
+        .lines()
+        .map(|l| l.split(","))
+        .map(|s| s.map(|i| i.split("-").map(|j| j.parse::<i32>().unwrap()).collect_tuple::<(_, _)>().unwrap()).collect_vec())
+        .filter(|r| overlaps(r[0], r[1]))
+        .count();
+
+    Solution::USize(overlaps_count)
+}
+
+fn fully_contains(a1: (i32, i32), a2: (i32, i32)) -> bool {
+    (a2.0 >= a1.0 && a2.1 <= a1.1) ||
+    (a1.0 >= a2.0 && a1.1 <= a2.1)
+}
+
+fn overlaps(a1: (i32, i32), a2: (i32, i32)) -> bool {
+    a1.0 <= a2.1 && a2.0 <= a1.1
 }
 
 #[cfg(test)]
@@ -16,8 +40,8 @@ mod tests {
 
     #[test]
     fn check() {
-        let input = read_string(2022, 5);
-        assert_eq!(part_one(&input), Solution::Empty);
-        assert_eq!(part_two(&input), Solution::Empty);
+        let input = read_string(2022, 4);
+        assert_eq!(part_one(&input), Solution::USize(530));
+        assert_eq!(part_two(&input), Solution::USize(903));
     }
 }
