c5038cb60a3807741c32d019f662919f093b5e2d
Cyrill Vetter <cyrill.vetter@gmail.com>
2022-12-07 10:16:01 +0100

    2022 day 6 improvements

--- src/year_2022/day_07.rs
+++ src/year_2022/day_07.rs
@@ -1,74 +1,66 @@
 use crate::solution::Solution;
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 pub fn part_one(input: &str) -> Solution {
-    const MAX_SIZE: u32 = 100000;
+    const MAX_SIZE: u32 = 100_000;
     let dirs = build_file_system(input);
 
     let file_sum = dirs
         .iter()
-        .map(|(k, _)| calculate_total_file_size(&dirs, k))
-        .filter(|sum| *sum < MAX_SIZE)
-        .sum::<u32>();
+        .map(|(k, _)| calculate_dir_size(&dirs, k))
+        .filter(|dir_size| *dir_size < MAX_SIZE)
+        .sum();
 
     Solution::U32(file_sum)
 }
 
 pub fn part_two(input: &str) -> Solution {
-    const TOTAL_DISK_SPACE: u32 = 70000000;
-    const NEEDED_FREE_SPACE: u32 = 30000000;
+    const TOTAL_DISK_SPACE: u32 = 70_000_000;
+    const NEEDED_FREE_SPACE: u32 = 30_000_000;
     let dirs = build_file_system(input);
 
-    let free_space = TOTAL_DISK_SPACE - calculate_total_file_size(&dirs, "/");
+    let free_space = TOTAL_DISK_SPACE - calculate_dir_size(&dirs, &PathBuf::from("/"));
 
     let smallest_dir = dirs
         .iter()
-        .map(|(k, _)| calculate_total_file_size(&dirs, k))
-        .filter(|sum| free_space + *sum > NEEDED_FREE_SPACE)
+        .map(|(k, _)| calculate_dir_size(&dirs, k))
+        .filter(|dir_size| free_space + *dir_size > NEEDED_FREE_SPACE)
         .min().unwrap();
 
     Solution::U32(smallest_dir)
 }
 
 struct Directory {
-    dirs: Vec<String>,
-    file_sizes: Vec<u32>
+    dirs: Vec<PathBuf>,
+    files_size: u32,
 }
 
-fn build_file_system(input: &str) -> HashMap<String, Directory> {
-    let mut dirs: HashMap<String, Directory> = HashMap::new();
-    let mut curr: String = String::from("/");
-    dirs.insert(curr.clone(), Directory { dirs: Vec::new(), file_sizes: Vec::new() });
+fn build_file_system(input: &str) -> HashMap<PathBuf, Directory> {
+    let mut dirs: HashMap<PathBuf, Directory> = HashMap::new();
+    let mut curr: PathBuf = PathBuf::from("/");
+    dirs.insert(curr.clone(), Directory { dirs: Vec::new(), files_size: 0 });
 
     for l in input.lines().skip(1) {
-        if l.starts_with("$") {
+        if l.starts_with('$') {
             let parts = l.split_whitespace().skip(1).collect_vec();
-            match parts[0] {
-                "cd" => {
-                    match parts[1] {
-                        ".." => {
-                            let last_separator = curr.rfind("/").unwrap();
-                            curr.replace_range(last_separator..curr.len(), "");
-                        },
-                        _ => move_in_dir(&mut curr, parts[1]),
-                    }
-                },
-                _ => (),
+            if parts[0] == "cd" {
+                match parts[1] {
+                    ".." => { curr.pop(); },
+                    _ => curr.push(parts[1]),
+                }
             }
         } else {
-            let parts = l.split_once(" ").unwrap();
+            let parts = l.split_once(' ').unwrap();
             match parts.0 {
                 "dir" => {
                     let mut current_dir = curr.clone();
-                    move_in_dir(&mut current_dir, parts.1);
+                    current_dir.push(parts.1);
                     dirs.get_mut(&curr).unwrap().dirs.push(current_dir.clone());
-                    dirs.insert(current_dir, Directory { dirs: Vec::new(), file_sizes: Vec::new() });
+                    dirs.insert(current_dir, Directory { dirs: Vec::new(), files_size: 0 });
                 },
-                _ => {
-                    let size = parts.0.parse::<u32>().unwrap();
-                    dirs.get_mut(&curr).unwrap().file_sizes.push(size);
-                }
+                _ => dirs.get_mut(&curr).unwrap().files_size += parts.0.parse::<u32>().unwrap(),
             }
         }
     }
@@ -76,20 +68,12 @@ fn build_file_system(input: &str) -> HashMap<String, Directory> {
     dirs
 }
 
-fn calculate_total_file_size(dirs: &HashMap<String, Directory>, dir_name: &str) -> u32 {
+fn calculate_dir_size(dirs: &HashMap<PathBuf, Directory>, dir_name: &PathBuf) -> u32 {
     let dir = dirs.get(dir_name).unwrap();
-    let mut total = dir.file_sizes.iter().sum::<u32>();
+    let mut total = dir.files_size;
     for childs in &dir.dirs {
-        total += calculate_total_file_size(dirs, &childs);
+        total += calculate_dir_size(dirs, childs);
     }
 
     total
-}
-
-fn move_in_dir(path: &mut String, dir: &str) {
-    if !path.ends_with("/") {
-        path.push_str("/");
-    }
-
-    path.push_str(dir);
 }
